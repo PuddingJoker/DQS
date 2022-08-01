f13d2329deccad5c682741df9e58f43206d75e4a
陈金鑫 <369790652@qq.com>
2022-08-01 17:52:19 +0800

    1.0.0

--- dist/extension.js
+++ dist/extension.js
@@ -1,1 +1,260 @@
-(()=>{"use strict";var e={579:(e,t,o)=>{Object.defineProperty(t,"__esModule",{value:!0});const r=o(496),n=r.commands.registerCommand("dqs.imp",(()=>{const e=r.window.activeTextEditor;if(!e)return;const t=/(\S+)(\.imp)$/,{selection:o,document:n}=e,s=n.getWordRangeAtPosition(o.anchor,t);if(!s)return r.window.showInformationMessage("您应该输入这种格式: xxx.imp");const i=n.getText(s),a=t.exec(i),c=a&&a[1],d=`import ${c} from "@/assets/images/${c}.png"`;e.edit((e=>{e.replace(s,d)})).then((()=>{const t=s.start.line,o=n.lineAt(t).firstNonWhitespaceCharacterIndex;e.selection=new r.Selection(new r.Position(t,d.length+o),new r.Position(t,d.length+o))}))}));t.default=n},108:(e,t,o)=>{Object.defineProperty(t,"__esModule",{value:!0});const r=o(496),n=o(147),s=r.commands.registerCommand("dqs.sort",(()=>{const e=r.window.activeTextEditor;if(!e)return;const t=/\s*import\s*(.+)\s*from\s*(.+)\s*\;*/g,{document:o}=e,s=o.fileName;let i=n.readFileSync(s,"utf8"),a=i.matchAll(t),c=0,d=[],p=[],f=[];for(let e of a)c++,-1!=e[2].indexOf("src/")||-1!=e[2].indexOf("assets/")?(f.push({dep:e[1],from:e[2]}),p.push({dep:e[1].replace(/\{|\}|\,/g,""),from:e[2]})):-1==e[2].indexOf("/")&&-1==e[2].indexOf(".")||-1!=e[2].indexOf("next/")||-1!=e[2].indexOf("nuxt/")?d.push({dep:e[1],from:e[2]}):(f.push({dep:e[1],from:e[2]}),p.push({dep:e[1].replace(/\{|\}|\,/g,""),from:e[2]}));function u(e,t){return e.sort(((e,o)=>{let r=e[t],n=o[t];return r>n?1:r<n?-1:0}))}const l=u(d,"dep"),m=u(p,"dep").map((e=>e.from)),x=[];for(let e=l.length-1;e>=0;e--){const t=l[e].from;/vue|react|angular/gi.test(t)?x.unshift(l[e]):x.push(l[e])}m.map((e=>{f.map((t=>{t.from===e&&x.push(t)}))}));let h="";x.map(((e,t)=>{const o=`\nimport ${e.dep} from ${e.from}`;h+=o})),h+="\n",a=i.replace(t,"*");let v="";for(let e=0;e<c;e++)v+="*";a=a.replace(v,h),n.writeFileSync(s,a)}));t.default=s},496:e=>{e.exports=require("vscode")},147:e=>{e.exports=require("fs")}},t={};function o(r){var n=t[r];if(void 0!==n)return n.exports;var s=t[r]={exports:{}};return e[r](s,s.exports,o),s.exports}var r={};(()=>{var e=r;Object.defineProperty(e,"__esModule",{value:!0}),e.deactivate=e.activate=void 0;const t=o(108),n=o(579);e.activate=function(e){e.subscriptions.push(t.default),e.subscriptions.push(n.default)},e.deactivate=function(){}})(),module.exports=r})();
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ([
+/* 0 */,
+/* 1 */
+/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+const vscode = __webpack_require__(2);
+const fs = __webpack_require__(3);
+let oldContent;
+const dqs = // 定义出发的命令，要和package.json的一致
+ vscode.commands.registerCommand("dqs.sort", () => {
+    // vs vode api
+    const currentEditor = vscode.window.activeTextEditor;
+    if (!currentEditor) {
+        return;
+    }
+    // 匹配 依赖引入的正则
+    const reg = /(import)(?:.*?(as))?(?:.*?(as))?(?:.*? (from))*.*/g;
+    const { document } = currentEditor;
+    // 获取当前执行的文件路径
+    const currentlyOpenTabfilePath = document.fileName;
+    // 读取内容
+    let fileContentArr = fs.readFileSync(currentlyOpenTabfilePath, "utf8");
+    // 避免换行符导致的bug
+    fileContentArr = fileContentArr.replace(/\{\n/g, "{");
+    fileContentArr = fileContentArr.replace(/\,\n/g, ",");
+    // newContent 是最后渲染的的内容 这里是匹配出所有的依赖句子
+    let newContent = fileContentArr.matchAll(reg);
+    // 统计一共有几句依赖，后续把依赖都替换成星号，然后再将星号替换成 newContent
+    let depLength = 0;
+    // 官方的 依赖数组  React、Vue等
+    let pureDepArr = [];
+    let originPureDepArr = [];
+    // 自身的依赖数组  "../../***"
+    let selfDepArr = [];
+    // 自身最原始的依赖数组
+    let originSelfDepArr = [];
+    // 遍历匹配的内容
+    for (let i of newContent) {
+        depLength++;
+        let impStr = i[0];
+        impStr = impStr.replace("import", "");
+        impStr = impStr.split("from");
+        if (impStr[1].indexOf("src/") != -1 ||
+            impStr[1].indexOf("assets/") != -1) {
+            originSelfDepArr.push({
+                dep: impStr[0],
+                from: impStr[1],
+            });
+            // 将除了字母以外的符号去掉，方便排序   {useEffect} => useEffect
+            selfDepArr.push({
+                dep: impStr[0]
+                    .replace(/\{|\}|\,/g, "")
+                    .trim()
+                    .toLocaleLowerCase(),
+                from: impStr[1],
+            });
+        }
+        // 判断是否是 官方的依赖
+        else if ((impStr[1].indexOf("/") == -1 && impStr[1].indexOf(".") == -1) ||
+            impStr[1].indexOf("next/") != -1 ||
+            impStr[1].indexOf("nuxt/") != -1) {
+            pureDepArr.push({
+                dep: impStr[0]
+                    .replace(/\{|\}|\,/g, "")
+                    .trim()
+                    .toLocaleLowerCase(),
+                from: impStr[1],
+            });
+            originPureDepArr.push({
+                dep: impStr[0],
+                from: impStr[1],
+            });
+        }
+        else {
+            originSelfDepArr.push({
+                dep: impStr[0],
+                from: impStr[1],
+            });
+            // 将除了字母以外的符号去掉，方便排序   {useEffect} => useEffect
+            selfDepArr.push({
+                dep: impStr[0]
+                    .replace(/\{|\}|\,/g, "")
+                    .trim()
+                    .toLocaleLowerCase(),
+                from: impStr[1],
+            });
+        }
+    }
+    // 单词排序
+    function alphabeticSorting(params, key) {
+        return params.sort((a, b) => {
+            let x = a[key].toLocaleLowerCase(), y = b[key].toLocaleLowerCase();
+            return x > y ? 1 : x < y ? -1 : 0;
+        });
+    }
+    const originDepArr2 = alphabeticSorting(pureDepArr, "dep");
+    const selfDepArr2 = alphabeticSorting(selfDepArr, "dep");
+    // 获取排序后的依赖顺序
+    const selfDepKeys = selfDepArr2.map((item) => [item.dep, item.from]);
+    const pureDepKeys = originDepArr2.map((item) => [item.dep, item.from]);
+    // 最后要渲染的依赖数组
+    const renderArr = [];
+    // 对官方的依赖进行排序  如果是 React等 就放在最前面
+    pureDepKeys.map(([dep, from]) => {
+        originPureDepArr.map((item) => {
+            if (item.from == from &&
+                item.dep
+                    .replace(/\{|\}|\,/g, "")
+                    .toLocaleLowerCase()
+                    .trim() == dep) {
+                if (/^vue|react|angular$/i.test(from)) {
+                    // console.log(item,renderArr);
+                    renderArr.unshift(item);
+                }
+                else {
+                    renderArr.push(item);
+                }
+            }
+        });
+    });
+    // 对自己的依赖进行排序
+    selfDepKeys.map(([dep, from]) => {
+        originSelfDepArr.map(item => {
+            if (item.from == from &&
+                item.dep
+                    .replace(/\{|\}|\,/g, "")
+                    .toLocaleLowerCase()
+                    .trim() == dep) {
+                renderArr.push(item);
+            }
+        });
+    });
+    // 最后的依赖顺序
+    let str = ``;
+    // 拼接字符串
+    renderArr.map((item, index) => {
+        const str3 = `\nimport ${item.dep.trim()} from ${item.from.trim()}`;
+        str += str3;
+    });
+    str += "\n";
+    // 将本身的语句全部替换为星号
+    newContent = fileContentArr.replace(reg, "*").replace(/\*\n*/g, "*");
+    //统计星号 再将星号替换成 最后的字符串
+    let replaceStr = "";
+    for (let i = 0; i < depLength; i++) {
+        replaceStr += "*";
+    }
+    newContent = newContent.replace(replaceStr, str);
+    if (oldContent && oldContent.trim() == newContent.trim()) {
+        return vscode.window.showInformationMessage(`当前的依赖无需再次排序   --from "DQS"`);
+    }
+    oldContent = newContent;
+    // 渲染
+    fs.writeFileSync(currentlyOpenTabfilePath, newContent);
+});
+exports["default"] = dqs;
+
+
+/***/ }),
+/* 2 */
+/***/ ((module) => {
+
+module.exports = require("vscode");
+
+/***/ }),
+/* 3 */
+/***/ ((module) => {
+
+module.exports = require("fs");
+
+/***/ }),
+/* 4 */
+/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+const vscode = __webpack_require__(2);
+const autoImp = vscode.commands.registerCommand("dqs.imp", () => {
+    const currentEditor = vscode.window.activeTextEditor;
+    if (!currentEditor) {
+        return;
+    }
+    const reg = /(\S+)(\.imp)$/;
+    const { selection, document } = currentEditor;
+    const position = document.getWordRangeAtPosition(selection.anchor, reg);
+    if (!position) {
+        return vscode.window.showInformationMessage("您应该输入这种格式: xxx.imp");
+    }
+    const docText = document.getText(position);
+    const tempArr = reg.exec(docText);
+    const prefix = tempArr && tempArr[1];
+    const replaceText = `import ${prefix} from "@/assets/images/${prefix}.png"`;
+    currentEditor
+        .edit(editer => {
+        editer.replace(position, replaceText);
+    })
+        .then(() => {
+        const line = position.start.line;
+        const index = document.lineAt(line).firstNonWhitespaceCharacterIndex;
+        currentEditor.selection = new vscode.Selection(new vscode.Position(line, replaceText.length + index), new vscode.Position(line, replaceText.length + index));
+    });
+});
+exports["default"] = autoImp;
+
+
+/***/ })
+/******/ 	]);
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+var exports = __webpack_exports__;
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.deactivate = exports.activate = void 0;
+const sort_ext_1 = __webpack_require__(1);
+const autoImp_ext_1 = __webpack_require__(4);
+function activate(context) {
+    context.subscriptions.push(sort_ext_1.default);
+    context.subscriptions.push(autoImp_ext_1.default);
+}
+exports.activate = activate;
+function deactivate() { }
+exports.deactivate = deactivate;
+
+})();
+
+module.exports = __webpack_exports__;
+/******/ })()
+;
+//# sourceMappingURL=extension.js.map
--- src/code/sort.ext.ts
+++ src/code/sort.ext.ts
@@ -1,6 +1,7 @@
 import * as vscode from "vscode";
 const fs = require("fs");
 
+let oldContent: string;
 const dqs = // 定义出发的命令，要和package.json的一致
   vscode.commands.registerCommand("dqs.sort", () => {
     // vs vode api
@@ -10,18 +11,22 @@ const dqs = // 定义出发的命令，要和package.json的一致
     }
 
     // 匹配 依赖引入的正则
-    const reg = /\s*import\s*(.+)\s*from\s*(.+)\s*\;*/g;
+    const reg = /(import)(?:.*?(as))?(?:.*?(as))?(?:.*? (from))*.*/g;
     const { document } = currentEditor;
     // 获取当前执行的文件路径
     const currentlyOpenTabfilePath = document.fileName;
     // 读取内容
     let fileContentArr = fs.readFileSync(currentlyOpenTabfilePath, "utf8");
+    // 避免换行符导致的bug
+    fileContentArr = fileContentArr.replace(/\{\n/g, "{");
+    fileContentArr = fileContentArr.replace(/\,\n/g, ",");
     // newContent 是最后渲染的的内容 这里是匹配出所有的依赖句子
     let newContent = fileContentArr.matchAll(reg);
     // 统计一共有几句依赖，后续把依赖都替换成星号，然后再将星号替换成 newContent
     let depLength = 0;
     // 官方的 依赖数组  React、Vue等
-    let originDepArr = [];
+    let pureDepArr: any = [];
+    let originPureDepArr: any = [];
     // 自身的依赖数组  "../../***"
     let selfDepArr = [];
     // 自身最原始的依赖数组
@@ -29,36 +34,56 @@ const dqs = // 定义出发的命令，要和package.json的一致
     // 遍历匹配的内容
     for (let i of newContent) {
       depLength++;
-      if (i[2].indexOf("src/") != -1 || i[2].indexOf("assets/") != -1) {
+      let impStr = i[0];
+      impStr = impStr.replace("import", "");
+      impStr = impStr.split("from");
+
+      if (
+        impStr[1].indexOf("src/") != -1 ||
+        impStr[1].indexOf("assets/") != -1
+      ) {
         originSelfDepArr.push({
-          dep: i[1],
-          from: i[2],
+          dep: impStr[0],
+          from: impStr[1],
         });
         // 将除了字母以外的符号去掉，方便排序   {useEffect} => useEffect
         selfDepArr.push({
-          dep: i[1].replace(/\{|\}|\,/g, ""),
-          from: i[2],
+          dep: impStr[0]
+            .replace(/\{|\}|\,/g, "")
+            .trim()
+            .toLocaleLowerCase(),
+          from: impStr[1],
         });
       }
       // 判断是否是 官方的依赖
       else if (
-        (i[2].indexOf("/") == -1 && i[2].indexOf(".") == -1) ||
-        i[2].indexOf("next/") != -1 ||
-        i[2].indexOf("nuxt/") != -1
+        (impStr[1].indexOf("/") == -1 && impStr[1].indexOf(".") == -1) ||
+        impStr[1].indexOf("next/") != -1 ||
+        impStr[1].indexOf("nuxt/") != -1
       ) {
-        originDepArr.push({
-          dep: i[1],
-          from: i[2]
+        pureDepArr.push({
+          dep: impStr[0]
+            .replace(/\{|\}|\,/g, "")
+            .trim()
+            .toLocaleLowerCase(),
+          from: impStr[1],
+        });
+        originPureDepArr.push({
+          dep: impStr[0],
+          from: impStr[1],
         });
       } else {
         originSelfDepArr.push({
-          dep: i[1],
-          from: i[2],
+          dep: impStr[0],
+          from: impStr[1],
         });
         // 将除了字母以外的符号去掉，方便排序   {useEffect} => useEffect
         selfDepArr.push({
-          dep: i[1].replace(/\{|\}|\,/g, ""),
-          from: i[2],
+          dep: impStr[0]
+            .replace(/\{|\}|\,/g, "")
+            .trim()
+            .toLocaleLowerCase(),
+          from: impStr[1],
         });
       }
     }
@@ -66,33 +91,50 @@ const dqs = // 定义出发的命令，要和package.json的一致
     // 单词排序
     function alphabeticSorting(params: any, key: any) {
       return params.sort((a: any, b: any) => {
-        let x = a[key],
-          y = b[key];
+        let x = a[key].toLocaleLowerCase(),
+          y = b[key].toLocaleLowerCase();
         return x > y ? 1 : x < y ? -1 : 0;
       });
     }
 
-    const originDepArr2 = alphabeticSorting(originDepArr, "dep");
+    const originDepArr2 = alphabeticSorting(pureDepArr, "dep");
     const selfDepArr2 = alphabeticSorting(selfDepArr, "dep");
     // 获取排序后的依赖顺序
-    const selfDepKeys = selfDepArr2.map((item: any) => item.from);
+    const selfDepKeys = selfDepArr2.map((item: any) => [item.dep, item.from]);
+    const pureDepKeys = originDepArr2.map((item: any) => [item.dep, item.from]);
     // 最后要渲染的依赖数组
-    const renderArr = [];
+    const renderArr: any = [];
 
     // 对官方的依赖进行排序  如果是 React等 就放在最前面
-    for (let i = originDepArr2.length - 1; i >= 0; i--) {
-      const from = originDepArr2[i].from;
-      if (/vue|react|angular/gi.test(from)) {
-        renderArr.unshift(originDepArr2[i]);
-      } else {
-        renderArr.push(originDepArr2[i]);
-      }
-    }
+    pureDepKeys.map(([dep, from]: any) => {
+      originPureDepArr.map((item: any) => {
+        if (
+          item.from == from &&
+          item.dep
+            .replace(/\{|\}|\,/g, "")
+            .toLocaleLowerCase()
+            .trim() == dep
+        ) {
+          if (/^vue|react|angular$/i.test(from)) {
+            // console.log(item,renderArr);
+            renderArr.unshift(item);
+          } else {
+            renderArr.push(item);
+          }
+        }
+      });
+    });
 
     // 对自己的依赖进行排序
-    selfDepKeys.map((key: any) => {
+    selfDepKeys.map(([dep, from]: any) => {
       originSelfDepArr.map(item => {
-        if (item.from === key) {
+        if (
+          item.from == from &&
+          item.dep
+            .replace(/\{|\}|\,/g, "")
+            .toLocaleLowerCase()
+            .trim() == dep
+        ) {
           renderArr.push(item);
         }
       });
@@ -102,14 +144,14 @@ const dqs = // 定义出发的命令，要和package.json的一致
     let str = ``;
 
     // 拼接字符串
-    renderArr.map((item, index) => {
-      const str3 =`\nimport ${item.dep} from ${item.from}`;
+    renderArr.map((item: any, index: any) => {
+      const str3 = `\nimport ${item.dep.trim()} from ${item.from.trim()}`;
       str += str3;
     });
-    str += '\n'
+    str += "\n";
 
     // 将本身的语句全部替换为星号
-    newContent = fileContentArr.replace(reg, "*");
+    newContent = fileContentArr.replace(reg, "*").replace(/\*\n*/g, "*");
 
     //统计星号 再将星号替换成 最后的字符串
     let replaceStr = "";
@@ -117,6 +159,13 @@ const dqs = // 定义出发的命令，要和package.json的一致
       replaceStr += "*";
     }
     newContent = newContent.replace(replaceStr, str);
+
+    if (oldContent && oldContent.trim() == newContent.trim()) {
+      return vscode.window.showInformationMessage(
+        `当前的依赖无需再次排序   --from "DQS"`
+      );
+    }
+    oldContent = newContent;
     // 渲染
     fs.writeFileSync(currentlyOpenTabfilePath, newContent);
   });
